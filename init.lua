6e0fdae66461d30ec2da117e316185ef168127ee
Bhaskar Dutta <bhaskardutta2209@gmail.com>
2026-06-01 11:13:17 +0530

    set tab default to 2 space

--- init.lua
+++ init.lua
@@ -617,6 +617,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        cssls = {},
 
         stylua = {}, -- Used to format Lua code
 
@@ -946,10 +947,10 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
 
--- lua/custom/vim-opts.lua
+++ lua/custom/vim-opts.lua
@@ -2,9 +2,9 @@ local M = {}
 
 M.setup = function()
   vim.opt.expandtab = true -- convert tab to spaces
-  vim.opt.tabstop = 2 -- Insert 2 space for a tab
-  vim.opt.shiftwidth = 2 -- Change the number of spaces inserted for indentation
-  vim.opt.softtabstop = 2 -- Make the space feel like a real tab when deleting
+  vim.opt.tabstop = 4 -- Insert 2 space for a tab
+  vim.opt.shiftwidth = 4 -- Change the number of spaces inserted for indentation
+  vim.opt.softtabstop = 4 -- Make the space feel like a real tab when deleting
 end
 
 return M
